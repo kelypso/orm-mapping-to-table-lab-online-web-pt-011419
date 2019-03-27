fbbe1c9fa4775a7ef81f492352699cdacd271734
Kelsey Low <kklowlow@gmail.com>
2019-03-27 01:27:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,9 @@ class Student
   attr_accessor :name, :grade, :id
   
   def initialize(name, grade, id=nil)
-    
+    @id = id
+    @name = name
+    @grade = grade
   end
   
   def self.create_table
